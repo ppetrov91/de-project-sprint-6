102a7cf77f5e7312af5395b7c5d1e7a5b168a041
Peter Petrov <p.petrov@postgrespro.ru>
2024-02-24 13:24:49 +0300

    Add comments.txt

--- dags/dds/sql/fill_s_auth_history.sql
+++ dags/dds/sql/fill_s_auth_history.sql
@@ -1,12 +1,15 @@
 INSERT INTO STV202311139__DWH.s_auth_history(hk_l_user_group_activity, user_id_from, event_name, event_dt, load_dt, load_src)
-SELECT HASH(u.hk_user_id, g.hk_group_id) AS hk_l_user_group_activity
+SELECT luga.hk_l_user_group_activity
      , gl.user_id_from
      , gl.event_name 
      , gl.event_ts AS event_dt
      , now() AS load_dt
      , 's3' AS load_src
   FROM STV202311139__STAGING.group_log gl
-  LEFT JOIN STV202311139__DWH.h_users u
-    ON u.user_id = gl.user_id
-  LEFT JOIN STV202311139__DWH.h_groups g
-    ON g.group_id = gl.group_id;
+  LEFT JOIN STV202311139__DWH.h_users hu
+    ON hu.user_id = gl.user_id
+  LEFT JOIN STV202311139__DWH.h_groups hg
+    ON hg.group_id = gl.group_id
+  LEFT JOIN STV202311139__DWH.l_user_group_activity luga 
+    ON hg.hk_group_id = luga.hk_group_id 
+   AND hu.hk_user_id = luga.hk_user_id;
